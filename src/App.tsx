57df1a3fea5e0961239f276f0d3644be95ced694
Greg Stoll <greg@gregstoll.com>
2019-03-15 22:17:16 -0500

    Show random color for TX at least

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import { StateMap, MapDateSlider, MapDate } from './lib';
+import 'rc-slider/assets/index.css';
+import 'semantic-ui-css/semantic.min.css';
+import { number } from 'prop-types';
 
-export class App extends Component<{}, {}> {
+interface AppState {
+    year: number,
+    fakeData: Map<number, Map<string, string>>
+}
+
+const MIN_YEAR = 2010;
+const MAX_YEAR = 2019;
+
+export class App extends Component<{}, AppState> {
+    state: AppState = {
+        year: MAX_YEAR,
+        fakeData: undefined
+    }
 
     onStateSelected = (stateCode: string) => {
     }
@@ -13,6 +28,25 @@ export class App extends Component<{}, {}> {
         alert("Error: " + this._errorStringFromError(error));
     }
 
+    randomColor = () => {
+        if (Math.random() * 2 < 1) {
+            return 'red';
+        }
+        return 'blue';
+    };
+
+    componentDidMount = () => {
+        // You probably want to load some data here, we'll just make something up.
+        let tempFakeData = new Map<number, Map<string, string>>();
+        for (let year = MIN_YEAR; year <= MAX_YEAR; ++year) {
+            let data = new Map<string, string>();
+            data.set('TX', this.randomColor());
+
+            tempFakeData.set(year, data);
+        }
+        this.setState({ fakeData: tempFakeData });
+    }
+
     _errorStringFromError = (error: any) => {
         // JS exceptions have this
         if (error.hasOwnProperty("message")) {
@@ -22,27 +56,32 @@ export class App extends Component<{}, {}> {
     }
 
     onSliderDateChange = (date: MapDate) => {
+        this.setState({ year: date.year });
     }
 
     render = () => {
-      return <div style={{ width: 640, margin: "15px auto" }}>
-        <StateMap isCartogram={true}
-            stateColors={new Map<string, string>()}
-            stateTitles={new Map<string, string>()}
-            stateSelectedCallback={this.onStateSelected}
-            stateClearedCallback={this.onStateCleared}
-            x={0}
-            y={0}
-            width={900}
-            height={500}
-            onError={this.onMapError} />
-          <MapDateSlider
-              yearsPerTick={1}
-              ticksPerYear={undefined}
-              startDate={new MapDate(2010, 11)}
-              endDate={new MapDate(2019, 11)}
-              currentDate={new MapDate(2019, 11)}
-              onDateChange={this.onSliderDateChange} />
+        if (this.state.fakeData === undefined) {
+            return <div>Loading</div>;
+        }
+        return <div style={{ width: 640, margin: "15px auto" }}>
+            <StateMap isCartogram={true}
+                stateColors={this.state.fakeData.get(this.state.year)}
+                stateTitles={new Map<string, string>()}
+                stateSelectedCallback={this.onStateSelected}
+                stateClearedCallback={this.onStateCleared}
+                x={0}
+                y={0}
+                width={900}
+                height={500}
+                onError={this.onMapError} />
+              <div>Year: {this.state.year}</div>
+              <MapDateSlider
+                  yearsPerTick={1}
+                  ticksPerYear={undefined}
+                  startDate={new MapDate(MIN_YEAR, 11)}
+                  endDate={new MapDate(MAX_YEAR, 11)}
+                  currentDate={new MapDate(this.state.year, 11)}
+                  onDateChange={this.onSliderDateChange} />
         </div>
     }
 }
